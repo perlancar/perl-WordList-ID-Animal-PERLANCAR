11256f09180924ef8147f908c4eebce5eba3b85c
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2020-05-23 14:16:14 +0700

    Initial commit

---
+++ lib/WordList/ID/Animal/PERLANCAR.pm
@@ -0,0 +1,245 @@
+package WordList::ID::Animal::PERLANCAR;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use WordList;
+our @ISA = qw(WordList);
+
+# STATS
+
+1;
+# ABSTRACT: List of animals in Indonesian
+
+=head1 DESCRIPTION
+
+Suitable for playing hangman.
+
+Some spelling variations included (e.g. C<orangutan> and C<orang utan>, C<onta>
+and C<unta>). Multiple popular names for the same animals include (e.g.
+C<kalong> and C<kelelawar>, C<lebah> and C<tawon>, C<dolfin> and
+C<lumba-lumba>). Some two-word animal names included, especially if they refer
+to distinct species or has popular single-word English equivalents (e.g. C<orang
+utan>, C<kuda laut>, C<kuda nil>, C<burung hantu>).
+
+
+=head1 SEE ALSO
+
+=cut
+
+__DATA__
+agas
+alap-alap
+albatros
+aligator
+anai-anai
+anakonda
+angsa
+anjing
+anjing laut
+anoa
+armadilo
+arwana
+ayam
+babi
+babi celeng
+babirusa
+babon
+badak
+bajing
+bangau
+banteng
+bebek
+bekantan
+bekicot
+belalang
+belalang sembah
+belatung
+belibis
+belut
+beo
+berang-berang
+beruang
+beruang kutub
+beruk
+betet
+biawak
+bintang laut
+binturung
+biri-biri
+bison
+buaya
+bulubabi
+bunglon
+burung
+burung hantu
+burung onta
+burung unta
+cacing
+camar
+caplak
+capung
+celeng
+cendrawasih
+cerpelai
+cicak
+citah
+cumi-cumi
+cupang
+curut
+dinosaurus
+dodo
+dolfin
+domba
+dubuk
+dugong
+elang
+flamingo
+gajah
+gorila
+gurita
+hamster
+harimau
+hiena
+hiu
+ibex
+ibis
+ibis
+iguana
+ikan
+impala
+itik
+jaguar
+jalak
+jangkrik
+jerapah
+kadal
+kaki seribu
+kalajengking
+kalkun
+kalong
+kambing
+kancil
+kangguru
+kapibara
+kasuari
+katak
+kecoa
+kecoak
+kelabang
+keledai
+kelelawar
+kelinci
+keong
+kepik
+kepiting
+kera
+kerang
+kerbau
+kijang
+koala
+kodok
+komodo
+kucing
+kuda
+kuda laut
+kuda nil
+kumbang
+kunang-kunang
+kungkang
+kupu-kupu
+kura-kura
+kuskus
+kutu
+kutu busuk
+laba-laba
+lalat
+landak
+laron
+lebah
+lele
+lembu
+lemur
+lintah
+lipan
+lipas
+llama
+lobster
+lumba-lumba
+lutung
+luwak
+macan
+macan kumbang
+macan tutul
+maleo
+mamut
+marmot
+marmut
+merak
+merpati
+mirkat
+monyet
+musang
+ngengat
+nuri
+nyamuk
+onta
+orang utan
+orangutan
+panda
+pari
+paus
+pelanduk
+pelatuk
+pelikan
+penyu
+perkutut
+pesut
+pinguin
+piranha
+platipus
+puma
+rayap
+rubah
+rusa
+salamander
+salmon
+sapi
+semut
+serigala
+siamang
+sigung
+simpanse
+singa
+singa laut
+siput
+sotong
+tapir
+tarantula
+tawon
+teripang
+tikus
+tikus tanah
+tiram
+tokek
+tomcat
+trenggiling
+tripang
+tupai
+ubur-ubur
+udang
+ular
+ulat
+undur-undur
+ungko
+unta
+walabi
+walet
+walrus
+wereng
+wombat
+yak
+zebra
+zebu
